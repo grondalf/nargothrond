1d52850fd06569c83ef1809299db2e3bad0f15c7
Vuk <vhidal01@ucm.es>
2025-08-31 14:49:39 +0200

    Update disable-repos.sh

--- files/scripts/repos/disable-repos.sh
+++ files/scripts/repos/disable-repos.sh
@@ -4,7 +4,7 @@ REPOS="/etc/yum.repos.d/"
 
 set -eoux pipefail
 
-dnf config-manager setopt fedora-cisco-openh264.enabled=0
+rm $REPOS/fedora-cisco-openh264.repo
 rm $REPOS/google-chrome.repo
 rm $REPOS/rpmfusion-nonfree-nvidia-driver.repo
 rm $REPOS/rpmfusion-nonfree-steam.repo
