5a38ff66b9ec44bc8aa3adc6e24e8d339a8717d1
Grondalf <grondalf@proton.me>
2025-09-03 01:07:38 +0200

    Added NVIDIA support as an akmod module.

--- files/scripts/remove-remnants.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-set -eoux pipefail
-
-rm remove-repos.sh
-rm setup-*.sh
--- files/scripts/setup-papirus-icons.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-wget -qO- https://github.com/grondalf/papirus-icon-theme/blob/master/install.sh | sh
-wget -qO- https://github.com/grondalf/papirus-folders/blob/master/install.sh | sh
-
-papirus-folders -C adwaita --theme Papirus
-papirus-folders -C adwaita --theme Papirus-Light
-papirus-folders -C adwaita --theme Papirus-Dark
