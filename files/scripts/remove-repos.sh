87731999957ba1236ab0685838193c03db286b98
Vuk <vhidal01@ucm.es>
2025-09-06 01:14:58 +0200

    Update remove-repos.sh

--- files/scripts/remove-repos.sh
+++ files/scripts/remove-repos.sh
@@ -4,7 +4,6 @@ REPOS="/etc/yum.repos.d/"
 
 set -eoux pipefail
 
-rm $REPOS/fedora-cisco-openh264.repo
 rm $REPOS/google-chrome.repo
 rm $REPOS/rpmfusion-nonfree-nvidia-driver.repo
 rm $REPOS/rpmfusion-nonfree-steam.repo
