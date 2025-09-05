96315b611c185c45d92e63b807eadced8d861d5a
grondalf <grondalf@proton.me>
2025-09-05 16:32:30 +0200

    Autofirma now  checks the md5 file provided by the repo

--- files/scripts/setup-autofirma.sh
+++ files/scripts/setup-autofirma.sh
@@ -6,7 +6,6 @@ set -oue pipefail
 # Your code goes here.
 echo "Downloading dependencies"
 curl -O https://firmaelectronica.gob.es/content/dam/firmaelectronica/descargas-software/autofirma19/Autofirma_Linux_Fedora.zip
-curl -L https://raw.githubusercontent.com/franute/nimbus-os/refs/heads/main/files/scripts/autofirma/autofirma.md5 > autofirma.md5
 unzip Autofirma_Linux_Fedora.zip
 
 if md5sum -c setup-autofirma.md5 > /dev/null; then
