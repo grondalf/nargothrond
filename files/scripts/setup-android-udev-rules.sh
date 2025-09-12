93749a87d34b683d5b2dc66f4922ac8c411c179f
Vuk <vhidal01@ucm.es>
2025-09-12 10:17:56 +0000

    Update setup-android-udev-rules.sh

--- files/scripts/setup-android-udev-rules.sh
+++ files/scripts/setup-android-udev-rules.sh
@@ -2,4 +2,9 @@
 
 set -eoux pipefail
 
-sudo ln -s /usr/share/doc/android-tools/51-android.rules /etc/udev/rules.d
+# Check if the symlink or target file exists before adding
+if [ ! -e /etc/udev/rules.d/51-android.rules ]; then
+    sudo ln -s /usr/share/doc/android-tools/51-android.rules /etc/udev/rules.d/51-android.rules
+else
+    echo 'Link or file already exists'
+fi
