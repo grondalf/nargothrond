fe9a459eb61a8441e3a2e31aebae477202be5a2c
Vuk <vhidal01@ucm.es>
2025-09-16 08:00:39 +0200

    Update remove-repos.sh

--- files/scripts/remove-repos.sh
+++ files/scripts/remove-repos.sh
@@ -4,17 +4,21 @@ REPOS="/etc/yum.repos.d/"
 
 set -eoux pipefail
 
-echo "Deleting unnecessary dnf repos."
+echo "Removing unused dnf repos."
 rm $REPOS/google-chrome.repo
 rm $REPOS/rpmfusion-nonfree-nvidia-driver.repo
 rm $REPOS/rpmfusion-nonfree-steam.repo
 
 echo "Replacing Fedora's default Flatpak repo with the unfiltered one."
-flatpak remote-delete fedora
+
+flatpak remotes | grep -qx "fedora" && flatpak remote-delete fedora
+
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 flatpak remote-modify --no-filter --enable flathub
 
 echo "Reinstalling default applications."
+
 flatpak install --reinstall --system --assumeyes flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application)
 
 echo "Unnecessary repos have been deleted." 
