0a965c0a2f62420d57f42a0653451d256af5c9e2
Vuk <vhidal01@ucm.es>
2025-09-16 15:57:37 +0200

    Update remove-repos.sh

--- files/scripts/remove-repos.sh
+++ files/scripts/remove-repos.sh
@@ -9,17 +9,3 @@ rm $REPOS/google-chrome.repo
 rm $REPOS/rpmfusion-nonfree-nvidia-driver.repo
 rm $REPOS/rpmfusion-nonfree-steam.repo
 
-echo "Replacing Fedora's default Flatpak repo with the unfiltered one."
-
-flatpak remotes | grep -qx "fedora" && flatpak remote-delete fedora
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak remote-modify --no-filter --enable flathub
-
-echo "Reinstalling default applications."
-
-flatpak install --reinstall --system --assumeyes flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application)
-
-echo "Unnecessary repos have been deleted." 
-
