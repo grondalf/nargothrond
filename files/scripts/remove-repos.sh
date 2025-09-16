01aa683e7d970a8fe99f63dbea81fb000b76723d
Vuk <vhidal01@ucm.es>
2025-09-16 20:55:52 +0200

    Update remove-repos.sh

--- files/scripts/remove-repos.sh
+++ files/scripts/remove-repos.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-REPOS="/etc/yum.repos.d/"
+REPOS="/etc/yum.repos.d"
 
-set -eoux pipefail
+set -eou pipefail
 
 echo "Removing unused dnf repos."
 rm $REPOS/google-chrome.repo
 rm $REPOS/rpmfusion-nonfree-nvidia-driver.repo
 rm $REPOS/rpmfusion-nonfree-steam.repo
+
+echo "Dome!" 
 
