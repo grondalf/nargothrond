c228a67dfbedff90227de4f6ebf5a45ae709d8f9
grondalf <grondalf@proton.me>
2025-10-12 22:55:17 +0200

    Forgot a few files.

--- files/scripts/setup-gamemode.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-# Run gamemode with the dedicated NVIDIA GPU
-
-echo 'GAMEMODERUNEXEC="env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"' | sudo tee -a /etc/environment
