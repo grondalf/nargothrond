eac3a85621f6c427d6b39ebd4495235618713ace
grondalf <grondalf@proton.me>
2025-11-02 17:36:28 +0100

    Added Hardcoded-tray icons support

--- files/scripts/setup-papirus.sh
+++ files/scripts/setup-papirus.sh
@@ -1,16 +1,44 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -euo pipefail
 
-set -eoux pipefail
+print_title() {
+    echo -e "\e[1;35m=========================================\e[0m"
+    echo -e "\e[1;35m Papirus & Hardcode-Tray Icons Installer \e[0m"
+    echo -e "\e[1;35m=========================================\e[0m"
+}
+                                                                                                  
+print_title
 
 echo "Downloading and installing Papirus icon theme from source..."
-wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sh
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sudo sh
 
 echo "Downloading and installing papirus-folders from source..."
-wget -qO- https://git.io/papirus-folders-install | sh
+wget -qO- https://git.io/papirus-folders-install | sudo sh
 
-echo "Setting adwaita papirus folders theme"
+echo "Setting Adwaita Papirus folders theme..."
 papirus-folders -C adwaita --theme Papirus
 papirus-folders -C adwaita --theme Papirus-Light
 papirus-folders -C adwaita --theme Papirus-Dark
 
-echo "Both Papirus icon theme and papirus-folders have been installed."
+echo "Installing dependencies for building Hardcode-Tray icons..."
+sudo dnf install -y git meson gdk-pixbuf2-devel gtk3-devel python3 python3-gobject gobject-introspection-devel librsvg2-tools ninja-build
+
+echo "Cloning Hardcode-Tray repository..."
+git clone https://github.com/bilelmoussaoui/Hardcode-Tray.git
+cd Hardcode-Tray
+
+echo "Compiling Hardcode-Tray..."
+meson builddir --prefix=/usr
+
+echo "Installing Hardcode-Tray..."
+sudo ninja -C builddir install
+
+echo "Cleaning up cloned repository..."
+cd ..
+rm -rf Hardcode-Tray
+
+echo "Removing unnecessary build dependencies..."
+sudo dnf remove -y meson gdk-pixbuf2-devel gtk3-devel gobject-introspection-devel ninja-build
+
+echo -e "\e[1;32mInstallation complete: Papirus icon themes and Hardcode-Tray installed. Build dependencies removed.\e[0m"
+
